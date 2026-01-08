2dc669a47ddf8138a96cc6c277b5529ffa2e8ff6
Romina Ramon <rominaramonp@gmail.com>
2026-01-07 21:52:45 -0300

    Arreglo como se ve la web, commento propiedades en body de index.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,24 +2,23 @@ import CartWidget from "./CartWidget";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <a className="navbar-brand" href="#">
-        Rofux
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid px-4">   
+        <a className="navbar-brand" href="#">
+            Rofux
+        </a>
 
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" href="#">Remeras</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pantalones</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Bolsos</a>
-        </li>
-      </ul>
-
-      <CartWidget />
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+            <a className="nav-link" href="#">Remeras</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">Pantalones</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">Bolsos</a>
+            </li>
+        </ul>
+        <CartWidget />
     </nav>
   );
 };
