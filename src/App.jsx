f39abb42028be2a726ef21f3ed9e984ffdaa7681
Romina Ramon <rominaramonp@gmail.com>
2026-01-28 15:45:41 -0300

    Agrego ruteo en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer mensaje="¡Bienvenido a nuestra tienda!" />
+
+      <Routes>
+        {/*Catalogo principal de productos*/}
+        <Route path="/" element={<ItemListContainer />} /> 
+
+        {/*Catálogo de productos filtrados por categorías */}
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+
+        {/* Vista en detalle de un producto */}
+        <Route path="/item/:id" element={<ItemListContainer />} />
+
+        {/* Ruta de tipo “404” */}
+        <Route path="*"  element={<h2>404 - Página no encontrada</h2>}/>
+
+      </Routes>
     </>
   );
 }
