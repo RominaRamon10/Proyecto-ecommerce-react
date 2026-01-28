23d13f59af1c649cd6d4cea99fc44ced28f8ded7
Romina Ramon <rominaramonp@gmail.com>
2026-01-28 16:54:11 -0300

    agrego obtener prod por categoria y lo separo de obtener porductos

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
--- src/services/mocks/products.js
+++ src/services/mocks/products.js
@@ -1,35 +1,53 @@
 import productos from "../../data/productos.json";
 
-//obtener productos por categoria
-const getProducts = (categoryId) => {
+//obtener productos
+const getProducts = () => {
     return new Promise((resolve, reject) => {
         const operaciónExitosa = true; // Simulación de éxito o fracaso
-        setTimeout(()=>{
-            if(categoryId){
-                if(operaciónExitosa){
-                    resolve({success: true, 
-                            data:(productos.filter(prod => prod.category === categoryId))})
-                } else{
-                    reject({success: false, message: "Error al obtener los productos"})
-                }
-            } else{
-                resolve(productos);
+        setTimeout(() => {
+            if (operaciónExitosa) {
+                resolve({ success: true, data: productos });
+            } else {
+                reject({ success: false, message: "Error al obtener los productos" });
             }
         }, 800);
     });
 };
 
+//obtener productos por categoria
+const getProductsByCategory = (categoryId) => {
+    return new Promise((resolve, reject) => {
+        const operaciónExitosa = true; // Simulación de éxito o fracaso
+        setTimeout(() => {
+            if (categoryId) {
+                if (operaciónExitosa) {
+                    resolve({
+                        success: true,
+                        data: productos.filter((prod) => prod.category === categoryId),
+                    });
+                } else {
+                    reject({ success: false, message: "Error al obtener los productos" });
+                }
+            }
+        }, 800);
+    });
+};
+
+//obtener producto por id
 const getProductById = (id) => {
     return new Promise((resolve, reject) => {
         const operaciónExitosa = true;
         setTimeout(() => {
-            if(operaciónExitosa){
-                resolve({success: true, data: (productos.find(prod => prod.id === id))})
-            } else{
-                reject({success: false, message: "Error al obtener el producto"})
+            if (operaciónExitosa) {
+                resolve({
+                    success: true,
+                    data: productos.find((prod) => prod.id === id),
+                });
+            } else {
+                reject({ success: false, message: "Error al obtener el producto" });
             }
         }, 800);
-    })
+    });
 };
 
-export const products = { getProducts, getProductById };
+export const products = { getProducts, getProductsByCategory, getProductById };
