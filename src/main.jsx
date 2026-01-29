103b9e3fd7a9715ea7746dfcc8694b93fac03229
Romina Ramon <rominaramonp@gmail.com>
2026-01-29 17:32:59 -0300

    Instalo PrimeReact y los importo en main

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+//Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter } from 'react-router-dom';
 
-
+//PrimeReact
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'  
+import 'primeicons/primeicons.css'  
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
