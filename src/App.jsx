d84b144fa14422c605277aa71410866f485be8db
Romina Ramon <rominaramonp@gmail.com>
2026-02-26 22:42:13 -0300

    Añado rutas del carrito y el checkoutForm a app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { Routes, Route } from "react-router-dom";
+import Cart from './components/Cart/Cart';
+import CheckoutForm from "./components/Cart/CheckoutForm";
 
 function App() {
   return (
@@ -32,6 +34,8 @@ function App() {
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/item/:id" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} /> 
+        <Route path="/checkout" element={<CheckoutForm />} />
         <Route path="*"  element={<h2>404 - Página no encontrada</h2>}/>
       </Routes>
     </> 
